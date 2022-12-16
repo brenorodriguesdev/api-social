3312459092e4016f0f44f30f192ad778eacad3f5
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-12-16 17:36:45 -0300

    feat: add sign up service

---
+++ src/services/sign-up.ts
@@ -0,0 +1,25 @@
+import { getRepository } from 'typeorm'
+import { User } from '../entities/user'
+import { crypt } from '../utils/hash'
+
+interface SignUpServiceProps {
+  email: string
+  name: string
+  password: string
+}
+
+export const signUpService = async ({ email, name, password }: SignUpServiceProps): Promise<void | Error> => {
+  const userRepository = getRepository(User)
+  const isEmailAlreadyExist = await userRepository.find({ where: { email } })
+  if (isEmailAlreadyExist) {
+    return new Error('Esse e-mail já está em uso!')
+  }
+
+  const user = new User()
+  user.email = email
+  user.name = name
+  const passwordHashed = await crypt(password)
+  user.password = passwordHashed
+
+  await userRepository.save(user)
+}
