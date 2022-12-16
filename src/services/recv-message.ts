5678fdb4d7cf6b370b6bf4f47907f9dfaff3acae
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-12-16 18:17:18 -0300

    feat: add recv message service

---
+++ src/services/recv-message.ts
@@ -0,0 +1,28 @@
+import { getRepository } from 'typeorm'
+import { ChatMessage } from '../entities/chat-message'
+import { User } from '../entities/user'
+
+interface RecvMessageServiceProps {
+  idChatMessage: number
+  idUserRecv: number
+}
+
+export const recvMessageService = async ({ idChatMessage, idUserRecv }: RecvMessageServiceProps): Promise<void | Error> => {
+  const userRepository = getRepository(User)
+  const userRecv = await userRepository.findOne({ where: { id: idUserRecv } })
+  if (!userRecv) {
+    return new Error('Esse usuário não existe!')
+  }
+  const chatMessageRepository = getRepository(ChatMessage)
+  const chatMessage = await chatMessageRepository.findOne({ where: { id: idChatMessage }, relations: ['chat', 'chat.other'] })
+  if (!chatMessage) {
+    return new Error('Essa mensagem não existe!')
+  }
+
+  if (chatMessage.chat.other.id !== userRecv.id) {
+    return new Error('Esse usuário não pode receber essa mensagem!')
+  }
+
+  chatMessage.recv_at = new Date()
+  chatMessageRepository.save(chatMessage)
+}
