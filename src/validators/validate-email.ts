e4aa8fceae30bbd408ede185119fb595b8edc47f
Breno Rodrigues <brenorodriguesdev@gmail.com>
2022-12-16 21:54:55 -0300

    fix: validate email

--- src/validators/validate-email.ts
+++ src/validators/validate-email.ts
@@ -1,5 +1,14 @@
+import isEmail from 'validator/lib/isEmail'
+
 export const validateEmail = (field: string, payload: any): void | Error => {
-  if (!payload[field]) {
+  const email = payload[field]
+  if (!email) {
+    return new Error(`${field} é inválido!`)
+  }
+
+  const isEmailValid = !isEmail(email)
+
+  if (!isEmailValid) {
     return new Error(`${field} é inválido!`)
   }
 }
